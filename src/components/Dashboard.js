70ec4bf95a79264e189461ffca0162b9f23d12f4
desko27 <desko27@gmail.com>
2018-02-10 14:16:15 +0100

    add dashboard component and move assets

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="Dashboard">
+        This is the dashboard!!
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
