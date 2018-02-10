b4838f15ee379a490cbdbb2795a9d1c1dc2eb07c
desko27 <desko27@gmail.com>
2018-02-10 15:49:59 +0100

    add material-ui

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Dashboard />
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <Dashboard />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { RaisedButton } from 'material-ui';
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="Dashboard">
-        This is the dashboard!!
+        <div>This is the dashboard!!</div>
+        <div><RaisedButton label="Click me!" /></div>
       </div>
     );
   }
